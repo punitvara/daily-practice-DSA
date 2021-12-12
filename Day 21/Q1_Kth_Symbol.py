e47b1b0fffabd243d23908adcde133c08a4e7668
vara27 <pvara@levi.com>
2021-12-12 13:44:06 +0530

    Create Q1_Kth_Symbol.py

---
+++ Day 21/Q1_Kth_Symbol.py
@@ -0,0 +1,81 @@
+'''
+Q1. Kth Symbol
+
+Problem Description
+
+On the first row, we write a 0. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+
+Given row A and index B, return the Bth indexed symbol in row A. (The values of B are 1-indexed.) (1 indexed).
+
+Problem Constraints
+
+1 <= A <= 20
+
+1 <= B <= 2A - 1
+
+
+
+Input Format
+
+First argument is an integer A.
+
+Second argument is an integer B.
+
+
+
+Output Format
+
+Return an integer denoting the Bth indexed symbol in row A.
+
+
+
+Example Input
+
+Input 1:
+
+ A = 2
+ B = 1
+Input 2:
+
+ A = 2
+ B = 2
+
+
+Example Output
+
+Output 1:
+
+ 0
+Output 2:
+
+ 1
+
+
+Example Explanation
+
+Explanation 1:
+
+ Row 1: 0
+ Row 2: 01
+Explanation 2:
+
+ Row 1: 0
+ Row 2: 01
+'''
+
+class Solution:
+
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        if A == 1:
+            return 0
+
+        mid = 2 **(A-1) // 2
+
+        if B <= mid:
+            return self.solve(A-1, B)
+        else:
+            # toggle 0 to 1 and 1 to 0 with ^
+            return  1 ^ self.solve(A-1, B-mid)
